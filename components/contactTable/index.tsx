210bdee1435ee641a05b85906073305ad9b7307c
Andy Davison <adavison@and.digital>
2020-09-16 15:28:15 +0100

    Add contact table component

---
+++ components/contactTable/index.tsx
@@ -0,0 +1,112 @@
+/**
+ * Copyright 2020 Inrupt Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { RDF, VCARD } from "@inrupt/lit-generated-vocab-common";
+import {
+  getFetchedFrom,
+  getThing,
+  getUrlAll,
+  removeUrl,
+  saveSolidDatasetAt,
+  setThing,
+  Url,
+  UrlString,
+} from "@inrupt/solid-client";
+import {
+  Table,
+  TableColumn,
+  useDataset,
+  useSession,
+  useThing,
+} from "@inrupt/solid-ui-react";
+import { Button, Typography } from "@material-ui/core";
+import styles from "./contactTable.module.css";
+
+export default function ContactTable({
+  property,
+}: {
+  property: Url | UrlString;
+}): React.ReactElement {
+  const { fetch } = useSession();
+  const { dataset } = useDataset();
+  const { thing: profile } = useThing();
+  const contactDetailUrls = getUrlAll(profile, property);
+  const contactDetailThings = contactDetailUrls.map((url) =>
+    getThing(dataset, url)
+  );
+
+  const saveHandler = async (newThing) => {
+    await saveSolidDatasetAt(
+      getFetchedFrom(dataset),
+      setThing(dataset, newThing),
+      { fetch }
+    );
+  };
+
+  const removePhone = async (index) => {
+    const contactDetailUrl = contactDetailUrls[index];
+    const newProfile = removeUrl(profile, property, contactDetailUrl);
+    await saveHandler(newProfile);
+    // TODO update local state or trigger re-fetching dataset
+  };
+
+  const CustomCell = ({ row: { index } }: { row: { index: number } }) => {
+    return (
+      <Button color="secondary" onClick={() => removePhone(index)}>
+        Delete
+      </Button>
+    );
+  };
+
+  return (
+    <Table things={contactDetailThings} className={styles.table}>
+      <TableColumn
+        property={RDF.type}
+        body={({ value }) => {
+          const shortValue = value.split("#").pop();
+          return <Typography>{shortValue || value}</Typography>;
+        }}
+        dataType="url"
+        header={() => (
+          <Typography>
+            <b>Type</b>
+          </Typography>
+        )}
+      />
+      <TableColumn
+        property={VCARD.value}
+        body={({ value }) => <Typography>{value}</Typography>}
+        dataType="url"
+        header={() => (
+          <Typography>
+            <b>Number</b>
+          </Typography>
+        )}
+      />
+      <TableColumn
+        property={VCARD.value}
+        body={CustomCell}
+        dataType="url"
+        header=""
+      />
+    </Table>
+  );
+}
