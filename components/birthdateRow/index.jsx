1bb287bf108caba0746d274391a8b4188c39b6cc
Virginia Balseiro <virginia.balseiro@inrupt.com>
2021-06-09 14:31:55 +0200

    Fix: update Birthdate component with changed to Value component
    - upgrade to latest version of `solid-ui-react` with the fix to the `Value` component
    
    - remove code that is not needed due to the `Value` component now handling lack of browser support for `datetime-local`

--- components/birthdateRow/index.jsx
+++ components/birthdateRow/index.jsx
@@ -18,7 +18,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-import { useState, useContext } from "react";
+import { useContext } from "react";
 import {
   useThing,
   useSession,
@@ -31,12 +31,10 @@ import {
   getSourceUrl,
   removeDatetime,
   saveSolidDatasetAt,
-  setDatetime,
   setThing,
 } from "@inrupt/solid-client";
 import { VCARD } from "@inrupt/lit-generated-vocab-common";
 import styles from "./birthdateRow.module.css";
-import { useEffect } from "react";
 
 export default function BirthdateRow({ edit, setEdit }) {
   const { solidDataset: dataset, setDataset } = useContext(DatasetContext);
@@ -44,30 +42,6 @@ export default function BirthdateRow({ edit, setEdit }) {
   const { fetch } = useSession();
   const { thing } = useThing(datasetUrl);
   const birthdate = thing && getDatetime(thing, VCARD.bday);
-  const [dateValue, setDateValue] = useState(
-    birthdate?.toISOString().slice(0, -8)
-  );
-  const [dateType, setDateType] = useState(true);
-  const [invalidForm, setInvalidForm] = useState(false);
-
-  useEffect(() => {
-    // checking that input type 'datetime-local' is supported
-    const test = document.createElement("input");
-    test.type = "datetime-local";
-    if (test.type === "text") {
-      setDateType(false);
-    }
-  });
-
-  async function handleSaveDate() {
-    const dateTimeRegex = /[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}/;
-    if (!dateTimeRegex.test(dateValue)) {
-      setInvalidForm(true);
-    } else {
-      setInvalidForm(false);
-      await addBirthdate(dateValue);
-    }
-  }
 
   async function removeBirthdate() {
     const newProfile = setThing(
@@ -80,46 +54,15 @@ export default function BirthdateRow({ edit, setEdit }) {
     setDataset(savedDataset);
   }
 
-  async function addBirthdate(date) {
-    const newProfile = setThing(
-      dataset,
-      setDatetime(thing, VCARD.bday, new Date(date))
-    );
-    const savedDataset = await saveSolidDatasetAt(datasetUrl, newProfile, {
-      fetch,
-    });
-    setDataset(savedDataset);
-  }
-
   return edit || birthdate ? (
     <span className={styles.birthdateRowWrapper}>
-      {(dateType || (!dateType && !edit)) && (
-        <Value
-          property={VCARD.bday}
-          dataType="datetime"
-          inputProps={{ name: "birthdate-input" }}
-          edit={edit}
-          autosave
-        />
-      )}
-      {edit && !dateType && (
-        // in case input type 'datetime-local' is not supported by the browser
-        <>
-          <input
-            name="birthdate-input"
-            type="text"
-            value={dateValue}
-            pattern={"[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}"}
-            onChange={(e) => setDateValue(e.target.value)}
-            onBlur={handleSaveDate}
-          />
-          {invalidForm && (
-            <span className={styles.alert}>
-              Please enter a date with the following format: yyyy-mm-ddThh:hh.
-            </span>
-          )}
-        </>
-      )}
+      <Value
+        property={VCARD.bday}
+        dataType="datetime"
+        inputProps={{ name: "birthdate-input" }}
+        edit={edit}
+        autosave
+      />
       {birthdate && !edit && (
         <Button
           color={"secondary"}
